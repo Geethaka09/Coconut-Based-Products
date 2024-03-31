f18b0b7fe7c9e9eb7d59ad3133163e22e22b0d44
Geethaka09 <160048680+Geethaka09@users.noreply.github.com>
2024-04-01 00:56:02 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let navbar = document.querySelector('.header .navbar a');
+let navbar = document.querySelector('.navbar');
 
-    document.querySelector('#menu-btn').onclick = () => {
-        console.log("Menu button clicked");
-        navbar.classList.toggle('active');
-    }
-});
+document.querySelector('#menu-btn').onclick = () => {
+    console.log("Menu button clicked");
+    navbar.classList.toggle('active');
+}
